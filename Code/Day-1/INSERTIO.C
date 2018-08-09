fe3a768e57503fb4ee7f08668a9dc419c0f22804
shrutisharma2604 <42046635+shrutisharma2604@users.noreply.github.com>
2018-08-09 22:01:07 +0530

    Insertion sort

---
+++ Code/Day-1/INSERTIO.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,key,count=0,a[100],n;
+	clrscr();
+	printf("enter the size of the array:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d\n",&a[i]);
+	}
+	count++;
+	for(j=2;j<=n;j++)
+	{
+		key=a[j]; count++;
+		i=j-1;    count++;
+		count++;
+		while(i>=0 && a[i]>key)
+		{
+			count++;
+			a[i+1]=a[i]; count++;
+			i=i-1; count++;
+		}
+		a[i+1]=key; count++;
+	}
+	printf("value of the count is=%d\n",count);
+	printf("The sorted array is:\n");
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+	getch();
+}
